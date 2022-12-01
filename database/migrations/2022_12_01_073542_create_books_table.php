94723c07fcbfa48cc043f8d594726fbf42dd8ad4
As <masiulis.evaldas@yahoo.co.uk>
2022-12-01 10:11:24 +0200

    'initial'

---
+++ database/migrations/2022_12_01_073542_create_books_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('books', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->text('description');
+            $table->string('ISBN', 20);
+            $table->string('photo')->nullable()->default(null);
+            $table->integer('pages');
+            $table->foreignId('category_id')->constrained();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('books');
+    }
+};
